ce43da9fb9aa037bdc354baa8e2f110e06bc6444
Laraib-Ejaz <laraibejaz04@gmail.com>
2026-01-06 00:38:37 +0500

    Update ChatBotActivity.java

--- app/src/main/java/com/example/nutrifit/ChatBotActivity.java
+++ app/src/main/java/com/example/nutrifit/ChatBotActivity.java
@@ -50,7 +50,7 @@ public class ChatBotActivity extends AppCompatActivity {
         currentUserEmail = userSession.getString("userEmail", "default_user");
 
         // 2. Gemini API Setup
-        GenerativeModel gm = new GenerativeModel("gemini-flash-latest", "AIzaSyCmrJ2wQAQcjjpTiA-mqgQCOMng3ff13bI");
+        GenerativeModel gm = new GenerativeModel("gemini-flash-latest", "api key");
         model = GenerativeModelFutures.from(gm);
 
         // 3. UI Initialization
